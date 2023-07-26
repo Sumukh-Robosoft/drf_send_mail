ccad711b27a2d0675c3686698b2ce2fba2543780
Sumukh-Robosoft <sumukh.r@robosoftin.com>
2023-07-26 15:58:14 +0530

    .env added

--- completeauth/settings.py
+++ completeauth/settings.py
@@ -127,17 +127,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "sumukh.robosoft@gmail.com"
-EMAIL_HOST_PASSWORD = "bdyaenxdwwtpxivf"
-
-
-
-VONAGE_API_KEY = "00e8fe4b"
-VONAGE_API_SECRET = "sSwQVuwBAmISvfO2"
-VONAGE_PHONE_NUMBER = '+919448745406'
 
 
